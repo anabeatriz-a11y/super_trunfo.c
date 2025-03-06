0b6022cfc9c194d4b8b8c7dec21c038e45bfcb7c
anabeatriz-a11y <anabeattiz8098097@gmail.com>
2025-03-06 20:55:24 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,41 +1,78 @@
+#include <stdio.h>
+
 int main() {
     char estado1, estado2;
-    char codigo1[3], codigo2[3];
+    char codigo1[4], codigo2[4];
     char nome1[50], nome2[50];
-    int populacao1, populacao2;
     float area1, area2;
+    int populacao1, populacao2;
     float PIB1, PIB2;
     int numeroDePontosTuristicos1, numeroDePontosTuristicos2;
 
-    printf ("CARTA 1 \n");
-    printf("escreva uma letra de A a H: ");
-    scanf ( " %c", &estado1);
-    printf ("escreva o codigo: ");
-    scanf ( " %s", &codigo1);
-    printf ("Nome Da Cidade: ");
-    scanf (" %s", &nome1);
-    printf ("populacao: ");
-    scanf (" %d", &populacao1);
-    printf ("PIB: ");
-    scanf (" %f", &PIB1);
-    printf ("Numeros De Pontos Turisticos: ");
-    scanf (" %d", &numeroDePontosTuristicos1);
-    printf ("Estado: %c \n Codigo: %s \n Nome da cidade: %s \n População: %d \n PIB: %f \n Numeros de pontos turisticos: %d \n", estado1, codigo1, nome1, populacao1, PIB1, numeroDePontosTuristicos1);
+    printf("CARTA 1\n");
+    printf("escreva uma letra de A a H: \n");
+    scanf(" %c", &estado1);
+    
 
-    printf ("CARTA 2 \n");
-    printf("escreva uma letra de A a H: ");
-    scanf ( " %c", &estado1);
-    printf ("escreva o codigo: ");
-    scanf ( " %s", &codigo2);
-    printf ("Nome Da Cidade: ");
-    scanf (" %s", &nome2);
-    printf ("populacao: ");
-    scanf (" %d", &populacao2);
-    printf ("PIB: ");
-    scanf (" %f", &PIB2);
-    printf ("Numeros De Pontos Turisticos: ");
-    scanf (" %d", &numeroDePontosTuristicos2);
-    printf ("Estado: %c \n Codigo: %s \n Nome da cidade: %s \n População: %d \n PIB: %f \n Numeros de pontos turisticos: %d \n", estado2, codigo2, nome2, populacao2, PIB2, numeroDePontosTuristicos2);
+    printf("escreva o codigo: \n");
+    scanf(" %3s", codigo1);
+    
+    printf("Nome Da Cidade: \n");
+    scanf(" %s", nome1);
+    
+
+    printf("Escreva a area: \n");
+    scanf(" %f", &area1);
+    
+
+    printf("populacao: \n");
+    scanf(" %d", &populacao1);
+    
+
+    printf("Escreva o PIB: \n");
+    scanf(" %f", &PIB1);
+    
+
+    printf("Numeros De Pontos Turisticos: \n");
+    scanf(" %d", &numeroDePontosTuristicos1);
+    
+
+    float pibpercapita1 = PIB1 / populacao1;
+    float densidade1 = populacao1 / area1;
+
+    printf("Estado: %c \n Codigo: %s \n Nome da cidade: %s \n Populacao: %d \n Area: %.3f km² \n PIB: %.3f \n Numeros de pontos turisticos: %d \n PIB Per Capita: %.3f \n Densidade Demografica: %.3f \n", estado1, codigo1, nome1, populacao1, area1, PIB1, numeroDePontosTuristicos1, pibpercapita1, densidade1);
+
+    printf("\nCARTA 2\n");
+    printf("escreva uma letra de A a H: \n");
+    scanf(" %c", &estado2);
+    
+    printf("escreva o codigo: \n");
+    scanf(" %3s", codigo2);
+    
+
+    printf("Nome Da Cidade: \n");
+    scanf(" %s", nome2);
+    
+    printf("Escreva a area: \n");
+    scanf(" %f", &area2);
+    
+
+    printf("populacao: \n");
+    scanf(" %d", &populacao2);
+    
+
+    printf("Escreva o PIB: \n");
+    scanf(" %f", &PIB2);
+    
+
+    printf("Numeros De Pontos Turisticos: \n");
+    scanf(" %d", &numeroDePontosTuristicos2);
+    
+
+    float pibpercapita2 = PIB2 / populacao2;
+    float densidade2 = populacao2 / area2;
+
+    printf ("Estado: %c \n Codigo: %s \n Nome da cidade: %s \n População: %d \n Area: %.3f km² \n PIB: %.3f \n Numeros de pontos turisticos: %d \n PIB Per Capita: %.3f \n Densidade Demografica: %.3f \n", estado2, codigo2, nome2, populacao2, area2, PIB2, numeroDePontosTuristicos2, pibpercapita2, densidade2);
 
     return 0;
 }
